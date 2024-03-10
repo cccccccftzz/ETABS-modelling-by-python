9e6f383f3e9b39f251b74fb463616e9aba8dbf6e
CHEN Fangting <cft0627@gmail.com>
2024-03-10 23:49:48 +0800

    Add eurocode concrete meterial function but imcomplete yet

--- material_prop.py
+++ material_prop.py
@@ -2,6 +2,7 @@
 Get all the materials from the created sap_model object
 To define the most common used concrete C30/37 from EC2 code for Singapore Industry Design
 '''
+import re
 
 def get_all_materials(sap_model):
     """
@@ -80,3 +81,81 @@ def set_concrete_material(sap_model, existing_prop, new_prop):
     is_lightweight = False
     stress_strain_curve_type = 2
     SSHysType 
+
+import re
+
+def add_eurocode_conc_materials(sap_model, delete_existing=False):
+    """
+    This will set all the concrete grades with material properties to Eurocode,
+    C12/15, C16/20, C20/25, C25/30, C30/37, C35/45, C40/50, C45/55, C50/60,
+    C55/67, C60/75, C70/85, C80/95, C90/105. The materials will have the
+    designation 'EC-C12/15' etc.
+
+    Parameters
+    SapModel : Pointer (refer to function connect_to_etabs)
+    delete_existing : Boolean. If True will delete all existing concrete
+                      materials
+
+    Returns None
+    """
+    conc_grades = [
+        "C12/15", "C16/20", "C20/25", "C25/30", "C30/37", "C35/45",
+        "C40/50", "C45/55", "C50/60", "C55/67", "C60/75", "C70/85",
+        "C80/95", "C90/105"
+    ]
+
+    gravity_weight = 25.0  # kN/m³
+
+    conc_mat_to_del = []
+    # Get existing concrete materials to be deleted
+    if delete_existing:
+        all_materials = get_all_materials(sap_model)
+        for mat in all_materials:
+            if all_materials[mat]["mat_type"] == "Concrete":
+                conc_mat_to_del.append(mat)
+
+    # Delete materials
+    for mat in conc_mat_to_del:
+        prop_del = sap_model.PropMaterial.Delete(mat)
+        if prop_del == 1:
+            print("Deleting material {} unsuccessful".format(mat))
+
+    # Add new Eurocode concrete materials
+    for grade in conc_grades:
+        conc_nm = "EC-" + grade
+        numeric_part = re.search(r'\d+', grade).group()  # Extract numeric part using regex
+        new_prop = sap_model.PropMaterial.AddMaterial(
+            conc_nm, 2, "User", "Eurocode", grade, UserName=conc_nm
+        )
+
+        # Check if the material was added successfully
+        if new_prop[1] != 0:
+            print("Adding material {} unsuccessful. Return code: {}".format(conc_nm, new_prop[1]))
+            continue
+
+        isLightweight = False
+        fcsFact = 0.0
+        SSType = 2
+        SSHysType = 4
+        strainAtFc = 0.003
+        strainAtUlt = 0.0035
+        return_code = sap_model.PropMaterial.SetOConcrete(
+            conc_nm,
+            float(numeric_part),  # Convert to float
+            isLightweight,
+            fcsFact,
+            SSType,
+            SSHysType,
+            strainAtFc,
+            strainAtUlt,
+        )
+
+        # Check if setting concrete properties was successful
+        if return_code != 0:
+            print("Setting properties for material {} unsuccessful. Return code: {}".format(conc_nm, return_code))
+            continue
+
+        # Rest of the code...
+        print("Material {} added successfully".format(conc_nm))
+
+    return None
